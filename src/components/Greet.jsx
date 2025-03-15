1d1c25b98591b3ece22532ed44e350ce3a118ed7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-15 17:30:28 +0100

    sprint

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Greet = () => {
-  return <div>hello people of God</div>;
+  return (
+    <div>
+      <Image />
+      <Title />
+    </div>
+  );
+};
+const Image = () => {
+  return <h2>Image palce holder</h2>;
+};
+const Title = () => {
+  return <h1>Ayodele</h1>;
 };
 
 export default Greet;
