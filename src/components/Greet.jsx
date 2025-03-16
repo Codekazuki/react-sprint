4803facc71fbde0602fb772f7dc5a85f7ef17406
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-16 12:45:29 +0100

    sprint

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-const Greet = () => {
+const Image =
+  "https://media.istockphoto.com/id/2161378701/photo/chimerella-mariaelenae-glass-frog-hanging-on-a-flower-stem.webp?a=1&b=1&s=612x612&w=0&k=20&c=A5HmQf_GMWC057V--B_5IBTaGqOZOIInFKAEY1993eQ=";
+const Title = "eat that frog";
+const Book = (props) => {
   return (
     <div>
-      <Image />
-      <Title />
+      <h1>{Title}</h1>
+      <img src={Image} alt={Title} />
+      <p>{props.job}</p>
     </div>
   );
 };
-const Image = () => {
-  return <h2>Image palce holder</h2>;
-};
-const Title = () => {
-  return <h1>Ayodele</h1>;
+const Greet = () => {
+  return (
+    <div>
+      <Book job='Success' />
+      <Book />
+    </div>
+  );
 };
 
 export default Greet;
