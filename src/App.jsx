8c37adb770b61a9e48bcee38383c8dd67f647867
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-16 22:17:32 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Booklist from "./components/Booklist";
-import Greet from "./components/Greet";
 
 function App() {
   return (
     <>
       <Booklist />
-      <Greet />
     </>
   );
 }
