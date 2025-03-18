60dadaf34a9a2a4a150c99e7db9139074793851b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-18 12:19:18 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Booklist from "./components/Booklist";
+import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
   return (
     <>
+      <ErrorExample />
       <Booklist />
     </>
   );
---
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -0,0 +1,5 @@
+const ErrorExample = () => {
+  return <h2>useState error exa</h2>;
+};
+
+export default ErrorExample;
