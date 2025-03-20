16b978e78db09966d515f2ab0e44636d4359f2d8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-20 21:46:31 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Booklist from "./components/Booklist";
-import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
-import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+
+import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
 
 function App() {
   return (
     <>
-      <UseStateBasics />
-      <Booklist />
+      <UseStateArray />
+      {/* <Booklist /> */}
     </>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClearList = () => {
+    setPeople([]);
+  };
+  const reloadList = () => {
+    setPeople(data);
+  };
+  const deleteFriend = (id) => {
+    setPeople(people.filter((friend) => friend.id !== id));
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <ul key={id}>
+            <li>{name}</li>
+            <button onClick={() => deleteFriend(id)}>Remove</button>
+          </ul>
+        );
+      })}
+
+      {people.length > 0 ? (
+        <button onClick={handleClearList}>ClearList</button>
+      ) : (
+        <button onClick={reloadList}>Reload List</button>
+      )}
+    </div>
+  );
 };
 
 export default UseStateArray;
