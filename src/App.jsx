2973d06360deeb265beae9f7091cad9177847c17
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-22 23:19:21 +0100

    sprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
 import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
 import "./index.css";
+import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
   return (
     <>
-      <UseStateArray />
+      <UseStateBasics />
     </>
   );
 }
